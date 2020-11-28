b10038a39c89bdb44b3b6acbf90a057e61bc89be
root <root@AIIH.localdomain>
2020-11-28 14:36:05 +0900

    v3 struct

--- go_struct_01.go
+++ go_struct_01.go
@@ -11,9 +11,23 @@ func showInfo(p part) {
 	fmt.Println("Description : ", p.description)
 	fmt.Println("Count : ", p.count)
 }
+
+func minimumOrder(description string) part {
+	var p part
+	p.description = description
+	p.count = 100
+	return p
+}
 func main() {
+	p := minimumOrder("Hex bolts")
+
+	fmt.Println(p.description, p.count)
+	/*
 	var bolts part
 	bolts.description = "Hex bolts"
 	bolts.count = 100
 	showInfo(bolts)
+	*/
+	
+
 }
